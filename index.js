f64ea79e109e597bfd7346e185cd5bef291d8e68
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-09-24 01:16:32 +0200

    set up controllers, db connection and routes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const router = require('./routes/Student.route');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.use(express.json());
+app.use('/api', router);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
