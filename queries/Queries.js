52c58e5698f1ce208d8bd55f6988acaf49718915
Collins Kipkemoi <chemerilcollins@gmail.com>
2024-09-26 15:28:08 +0200

    Add neptune_id field to insert student query
    The `addStudent` query was updated to include the `neptune_id` field. This ensures all necessary student data is captured in the database upon insertion.

--- queries/Queries.js
+++ queries/Queries.js
@@ -1,7 +1,7 @@
 const getAllStudents = 'SELECT * FROM STUDENTS'
 const studentById = 'SELECT * FROM STUDENTS WHERE id = $1'
 const checkEmailExists = 'SELECT * FROM STUDENTS WHERE EMAIL = $1'
-const addStudent = 'INSERT INTO STUDENTS (first_name, second_name, email, dob) VALUES ($1, $2, $3, $4) RETURNING *' // Return the newly created student
+const addStudent = 'INSERT INTO STUDENTS (first_name, second_name, email, dob, neptune_id) VALUES ($1, $2, $3, $4, $5) RETURNING *' // Return the newly created student
 const deleteStudent = 'DELETE FROM STUDENTS WHERE email = $1 RETURNING *'
 const updateStudent = 'UPDATE STUDENTS SET first_name = $1, second_name = $2, email = $3, dob = $4 WHERE id = $5 RETURNING *'
 module.exports = {
